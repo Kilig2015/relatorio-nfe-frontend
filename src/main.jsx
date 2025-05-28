e7775dc30fdf3ee7364b6928727b4d79452d0139
Kilig2015 <contato@kiligconsulting.com.br>
2025-05-28 10:39:53 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import UploadRelatorio from './UploadRelatorio';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <UploadRelatorio />
   </React.StrictMode>
-)
+);

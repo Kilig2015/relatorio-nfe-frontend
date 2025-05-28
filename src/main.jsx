01cf570333221c51818eec5f73d6434f7d5d63a5
Kilig2015 <contato@kiligconsulting.com.br>
2025-05-28 13:01:35 +0100

    Adicionar

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,1 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import UploadRelatorio from './UploadRelatorio.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<UploadRelatorio />);
+import React from 'react'; import ReactDOM from 'react-dom/client'; import UploadRelatorio from './UploadRelatorio'; ReactDOM.createRoot(document.getElementById('root')).render(<UploadRelatorio />);

3c6109b7083721937a0f42d7911e08693e2c492c
Kilig2015 <contato@kiligconsulting.com.br>
2025-05-28 11:01:40 +0100

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import UploadRelatorio from './UploadRelatorio';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<UploadRelatorio />);
